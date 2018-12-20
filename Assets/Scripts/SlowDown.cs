1c6253c2924a7dee6b7260cca09ec0e8cf495ac9
tdsummer <oliver.summermatter@stud.hslu.ch>
2018-12-20 15:02:12 +0100

    new SlowDown Prefab added

---
+++ Assets/Scripts/SlowDown.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDown : MonoBehaviour {
+    public int newSpeed;
+    private float normalSpeed;
+    private PlayerController playerController;
+
+    void Start() {
+        playerController = FindObjectOfType<PlayerController>();
+        normalSpeed = playerController.moveSpeed;
+
+    }
+
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.GetComponent<PlayerController>() != null) {
+            Debug.Log("SlowDown underground");
+            playerController.moveSpeed = newSpeed;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.GetComponent<PlayerController>() != null) {
+            playerController.moveSpeed = normalSpeed;
+        }
+    }
+}

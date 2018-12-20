b894aef6a661242d69af58cc0e01489ead895684
Alexander Morard <alexander.morard@stud.hslu.ch>
2018-12-20 22:47:58 +0100

    Added Game Ending, Text in Menus and a lot of stuff in the level

---
+++ Assets/Scripts/GameEndController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEndController : MonoBehaviour {
+
+    private void OnTriggerStay(Collider other) {
+        if (other.GetComponent<PlayerController>() != null) {
+            SceneManager.LoadScene(2);
+        }
+
+    }
+}

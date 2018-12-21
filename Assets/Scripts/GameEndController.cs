bb33afbe5a005c64e7fa50047c76151f6a515d6b
tdsummer <oliver.summermatter@stud.hslu.ch>
2018-12-21 14:54:04 +0100

    Score Implemented on Death

--- Assets/Scripts/GameEndController.cs
+++ Assets/Scripts/GameEndController.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
+
 public class GameEndController : MonoBehaviour {
+    // Use this for initialization
+    void Start() {
+
+    }
 
     private void OnTriggerStay(Collider other) {
         if (other.GetComponent<PlayerController>() != null) {
-            SceneManager.LoadScene(2);
+            //give highscore on winScript:
+            Debug.Log("Deathscore: "+ScoreManager.score);
+            wonScore.hScore = ScoreManager.score ;
+            SceneManager.LoadScene(3);
         }
 
     }
---
+++ Assets/Scripts/wonScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // Add the TextMesh Pro namespace to access the various functions.
+
+public class wonScore : MonoBehaviour {
+    public TextMeshProUGUI myScore;
+    public static int hScore;
+    
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Deathscore2: " + hScore);
+        if (myScore.tag == "deathscore") {
+            myScore.text = "Score: " + hScore;
+        }
+
+    }
+    public void SetScore(int score) {
+        hScore = score;
+    }
+
+}
